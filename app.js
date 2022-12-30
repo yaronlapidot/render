500edeaaff386d6fcba65d1c32bdea511365e626
Yaron Lapidot <ylapidot@netvision.net.il>
2022-12-30 10:40:22 +0200

    Added Morgan to project

--- app.js
+++ app.js
@@ -2,11 +2,13 @@
 require('dotenv').config();
 const express=require('express');
 const app=express();
+const morgan=require('morgan');
 const port=process.env.PORT || 3030;
 
+app.use(morgan('dev'));
 // הגדרנו נקודת קצה בשיטת גט שמחזירה הודעה של כל המוצרים נשלחו
 app.get('/products',(req,res)=>{
-    res.status(200).json({Msg:"All Products"});
+    res.status(200).json({Msg:"All Products new"});
 });
 
 app.listen(port,()=>{console.log('Server Started')});
